875e786f0293dcdb7a01607a5c1a77aa9920f4c1
Mohamad Khawam <mk1800@rutgers.edu>
2025-09-30 15:17:58 -0400

    Added more info for cas validation

--- src/lib/validators/cas-validator.ts
+++ src/lib/validators/cas-validator.ts
@@ -21,6 +21,22 @@ export interface CasValidationResponse {
 export const validate = async (validationUrl: string, ticket: string): Promise<CasUser> => {
     try {
         const response = await fetch(`${validationUrl}&ticket=${encodeURIComponent(ticket)}&format=json`);
+
+        if (!response.ok) {
+            throw new Error(`CAS validation request failed with status ${response.status}`);
+        }
+        if (response.headers.get('content-type')?.indexOf('application/json') === -1) {
+            throw new Error(
+                `CAS validation response is not JSON: 
+                ${response.headers.get('content-type')} 
+                data: ${await response.text()} 
+                validationUrl: ${validationUrl}`,
+                {
+                    cause: new Error('Invalid content-type in CAS response expected application/json')
+                }
+            );
+        }
+
         const data = (await response.json()) as CasValidationResponse;
 
         if ('authenticationFailure' in data.serviceResponse) {
@@ -32,7 +48,15 @@ export const validate = async (validationUrl: string, ticket: string): Promise<C
             user,
             attributes
         };
-    } catch {
+    } catch (err) {
+        if (err instanceof Error) {
+            throw new Error('Ticket failed validation: ' + err.message, { cause: err });
+        }
+        // if json parsing fails or any other error occurs
+        if (err instanceof SyntaxError) {
+            throw new Error('Ticket failed validation: Invalid JSON response' + err.message, { cause: err });
+        }
+
         throw new Error('Ticket failed validation');
     }
 };
